e2f25ca6bf52b68d74d62e18161c26402abce2f7
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2023-11-17 16:52:09 -0500

    feat(project): :sparkles: add HTML structure

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,22 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'soft-blue': 'hsl(215, 51%, 70%)',
+				'cyan': 'hsl(178, 100%, 50%)',
+				'very-dark-blue-main': 'hsl(217, 54%, 11%)',
+				'very-dark-blue-card': 'hsl(216, 50%, 16%)',
+				'very-dark-blue-line': 'hsl(215, 32%, 27%)',
+			},
+			backgroundColor: {
+				'soft-blue': 'hsl(215, 51%, 70%)',
+				'cyan': 'hsl(178, 100%, 50%)',
+				'very-dark-blue-main': 'hsl(217, 54%, 11%)',
+				'very-dark-blue-card': 'hsl(216, 50%, 16%)',
+				'very-dark-blue-line': 'hsl(215, 32%, 27%)',
+			}
+		},
 	},
 	plugins: [],
 }
